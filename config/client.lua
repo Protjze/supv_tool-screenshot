ff67f819706c2ac1b02cfd02936c951dd3120adb
SUP2Ak <cormier.wesley@gmail.com>
2023-04-19 04:04:38 +0200

    tweak: (config/client.lua) add richards_majectic

--- config/client.lua
+++ config/client.lua
@@ -1,8 +1,24 @@
 local options <const> = {
-    {value = 'mazebank', label = "Maze Bank"},
-    {value = 'weedfarm', label = "Weed Farm"},
-    {value = 'methlab', label = "Meth Labs"}
+    ['ipl'] = {
+        {value = 'mazebank', label = "Maze Bank"},
+        {value = 'weedfarm', label = "Weed Farm"},
+        {value = 'methlab', label = "Meth Labs"}, 
+        {value = 'richards_majectic', label = "Richards Majestic"},-- Richards Majestic
+    },
+
+    ['vehicle'] = {
+        {value = 'bike', label = 'Moto'},
+        {value = 'sports', label = 'Sports'},
+        {value = 'classic', label = "Classic"},
+    },
+
+    ['props'] = {
+        {value = 'furnitures', label = "Meubles"},
+        {value = 'chairs', label = "Chaises"}
+    }
 }
+
+local used <const> = 'ipl'
 
 return {
     command = {
@@ -10,9 +26,9 @@ return {
         takeScreen = 'st'
     },
 
-    input = {type = 'select', label = "Folder Name", options = options, required = true, icon = 'folder'}, -- or nil to set personnaly filename
+    input = {type = 'select', label = "Folder Name", options = options[used], required = true, icon = 'folder'}, -- or nil to set personnaly filename
 
     -- discord webhook, all is read and execute by client, please don't use this script on public server
-    sbasic = 'https://discord.com/api/webhooks/1037204560680329326/HYaU-', -- set webhook for screenshot-basic can upload
-    upload = 'https://discord.com/api/webhooks/1097364672501514293/' -- set webhook to upload with title + description
+    sbasic = 'https://discord.com/api/webhooks/1098038830378012683/3xicgMg82BdFAiW1snOwNwCHlUe2cnistvvf4H2AdHwBqDt2Kbjw6oDJuBWwfYOpdL7G', -- set webhook for screenshot-basic can upload
+    upload = 'https://discord.com/api/webhooks/1098038895226146846/piIi20ZwSId_cX4hNUmR_1DJ3m7YpgMYJlUrmW2fgF0dEfwhQYZh2AZFgfrTD4ssEeaK' -- set webhook to upload with title + description
 }
